3d4fa8c4d4d7a290d2dbd20664454dd8b5e54a75
Iggy Drougge <idrougge@mac.com>
2016-05-16 14:45:14 +0200

    Initial Commit

---
+++ TraderaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraderaTest
+//
+//  Created by Iggy Drougge on 2016-05-16.
+//  Copyright © 2016 Iggy Drougge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
